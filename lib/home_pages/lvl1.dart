5402e4b6d4cfe77ad1e68e2843f9c7557a10f4a5
INL Juno <gigih.hbs321@gmail.com>
2024-01-07 23:59:34 +0700

    nyicil v2

--- lib/home_pages/lvl1.dart
+++ lib/home_pages/lvl1.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,11 +15,68 @@ class _pageLvl1State extends State<pageLvl1> {
     return Scaffold(
       body: Center(
         child: Container(
-            child: Text('Ini Halaman 1',
-                style: GoogleFonts.plusJakartaSans(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700))),
+          child: FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('materi')
+                  .doc('U5hHWqaIPcFrQ7YwppNI')
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else {
+                  final user = snapshot.data!;
+                  return Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          user['title'],
+                          style: GoogleFonts.plusJakartaSans(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          user['contoh'],
+                          style: GoogleFonts.plusJakartaSans(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          user['deskripsi'],
+                          style: GoogleFonts.plusJakartaSans(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          user['arti'],
+                          style: GoogleFonts.plusJakartaSans(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              }),
+        ),
       ),
     );
   }
