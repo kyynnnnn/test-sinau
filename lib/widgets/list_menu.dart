679cb69e132752df4a2f83792e18ba84e3b00fc5
INL Juno <gigih.hbs321@gmail.com>
2024-01-08 18:51:18 +0700

    habis cots

--- lib/widgets/list_menu.dart
+++ lib/widgets/list_menu.dart
@@ -16,8 +16,16 @@ class ListViewCustom extends StatelessWidget {
               width: 361.0,
               height: 110.0,
               decoration: BoxDecoration(
-                border: Border.all(color: green, width: 2.0),
-                color: Colors.white,
+                //border: Border.all(color: green, width: 2.0),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.green,
+                    Colors.greenAccent,
+                  ],
+                ),
+                //color: Color.fromARGB(255, 138, 255, 21),
                 borderRadius: BorderRadius.circular(15.0),
               ),
               child: InkWell(
@@ -52,7 +60,7 @@ class ListViewCustom extends StatelessWidget {
                             style: GoogleFonts.plusJakartaSans(
                                 color: black,
                                 fontSize: 15.0,
-                                fontWeight: FontWeight.w700),
+                                fontWeight: FontWeight.w800),
                           ),
                         ],
                       )
